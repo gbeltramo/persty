c57c767f933520a9d4fffe27961a7631766f27d9
Gabriele Beltramo <gabri.beltramo@gmail.com>
2019-11-19 11:45:27 +0000

    new recursion implementation

--- persty/graph.py
+++ persty/graph.py
@@ -42,9 +42,9 @@ def indices_in_box(rect, points, sorted_indices):
 
 #=================================================
 
-def minibox_graph(points):
+def minibox_edges(points):
     """
-    Minibox graph of points in d-dimensional space with 
+    Minibox edges of points in d-dimensional space with 
     Chebyshev distance.
 
     Parameters
@@ -93,56 +93,48 @@ def remove_i_element(points, i):
 
 #=================================================
 
-def resize_hyperrectangle(hyperrectangle, proj_minibox, z, k):
-    recursion = False
-    bottom_k, top_k = hyperrectangle[k]
-    mini_bottom_k, mini_top_k = proj_minibox[k]
-    new_hyperrectangle = _np.copy(hyperrectangle)
-    
-    # slide in direction k if z[k] outside minibox in d-1 dimensions
-    if z[k] <= mini_bottom_k:
-        recursion = True
-        new_hyperrectangle[k][0] = max(z[k], bottom_k)
-        new_hyperrectangle[k][1] = top_k
-    elif mini_top_k <= z[k]:
-        recursion = True
-        new_hyperrectangle[k][0] = bottom_k
-        new_hyperrectangle[k][1] = min(z[k], top_k)
-    
-    return recursion, new_hyperrectangle
-
-#=================================================
-
-def exists_empty_hypercube(hyperrectangle, delta, 
+def exists_empty_hypercube(search_space, delta, 
                            proj_z_points, proj_minibox):
     """z_points and hyperrectangle are in d-1 dimensions"""
-
-    sides_large_enough = check_hyperrectangle_sides(hyperrectangle,
-                                                    delta)
+    
+    sides_large_enough = check_hyperrectangle_sides(search_space, delta)
     
     if not sides_large_enough:
         return False
     elif len(proj_z_points) == 0 and sides_large_enough:
         return True
     else:
-        dimension = len(proj_z_points[0])
-            
-        for i, z in enumerate(proj_z_points):
-            new_proj_z_points = remove_i_element(proj_z_points, i)
-            for k in range(dimension):
-                recursion, new_hyperrectangle = resize_hyperrectangle(hyperrectangle, proj_minibox, z, k)
-                
-                if recursion:
-                    return exists_empty_hypercube(new_hyperrectangle,
-                                                  delta, 
-                                                  new_proj_z_points, 
-                                                  proj_minibox)
+        # remove first point from those in search_space 
+        # and use it to split the search space recursively
+        z = proj_z_points[0]
+        new_proj_z_points = proj_z_points[1:]
+        # slide if z_k outside proj_minibox and within search_space
+        for k in range(len(z)):
+            bottom_k, top_k = search_space[k]
+            mini_bottom_k, mini_top_k = proj_minibox[k]
+            new_search_space = _np.copy(search_space)
+            if (bottom_k < z[k] <= mini_bottom_k):
+                new_search_space[k][0] = z[k]
+                if exists_empty_hypercube(new_search_space, delta, 
+                                          new_proj_z_points, proj_minibox):
+                    return True
+            elif (mini_top_k <= z[k] < top_k):
+                new_search_space[k][1] = z[k]
+                if exists_empty_hypercube(new_search_space, delta, 
+                                          new_proj_z_points, proj_minibox):
+                    return True
+            elif (z[k] <= bottom_k) or (top_k <= z[k]):
+                if exists_empty_hypercube(new_search_space, delta, 
+                                          new_proj_z_points, proj_minibox):
+                    return True
+        return False
 
 #=================================================
 
-def delaunay_graph(points):
+def delaunay_edges(points):
     """
-    Delaunay graph of points in d dimensional space with Chebyshev distance
+    Delaunay graph of points in d dimensional space with Chebyshev
+    distance.
     """
     
     delaunay_edges = []
@@ -159,6 +151,7 @@ def delaunay_graph(points):
         indices_in_minibox = indices_in_box(minibox,
                                             points,
                                             sorted_indices)
+        # if minibox of p,q empty, check with recursion
         if len(indices_in_minibox) == 0:
             K_delta, delta = find_delta(p, q)
             proj_minibox = remove_i_element(minibox, K_delta)
@@ -170,7 +163,6 @@ def delaunay_graph(points):
             indices_z_points = indices_z_points[indices_z_points!=j]
             search_space = remove_i_element(maxibox, K_delta)
             proj_z_points = _np.delete(points[indices_z_points], K_delta, axis=1)
-            
             if exists_empty_hypercube(search_space, delta,
                                       proj_z_points, proj_minibox):
                 delaunay_edges.append((i,j))
@@ -179,20 +171,20 @@ def delaunay_graph(points):
 
 #=================================================
 
-def delaunay_simplices(delaunay_edges, n, dim=3):
-    delaunay_simplices = []
+def flag_simplices(edges, n, dim=3):
+    simplices = []
     all_simplices = _combinations(range(n), dim)
     
     edges_in_simplex = list(_combinations(range(dim), 2))
     for k, sim in enumerate(all_simplices):
         sim_k_in = True
         for i, j in edges_in_simplex:
-            if (sim[i], sim[j]) not in delaunay_edges:
+            if (sim[i], sim[j]) not in edges:
                 sim_k_in = False
                 break
         if sim_k_in:
-            delaunay_simplices.append(sim)
-            
-    return delaunay_simplices
+            simplices.append(sim)
+    
+    return simplices
 
 #=================================================
