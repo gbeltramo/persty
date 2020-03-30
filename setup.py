01b068136f3f5ea2abb1e091eb1521115b6c4b9c
Gabriele Beltramo <gabri.beltramo@gmail.com>
2020-03-30 16:19:53 +0100

    new version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ with open("README.md", "r") as f:
 
 setup(
     name = "persty",
-    version = "0.2.2",
+    version = "0.2.3",
     author="Gabriele Beltramo",
     author_email="gabri.beltramo@gmail.com",
     description = "Implementation of Minibox and Delauany edges algorithms.",
