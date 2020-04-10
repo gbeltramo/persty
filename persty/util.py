8bc47e427627447c8068e75c22ebc480da17329a
Gabriele Beltramo <gabri.beltramo@gmail.com>
2020-04-10 19:17:47 +0200

    use sim_tree.expansion()

--- persty/util.py
+++ persty/util.py
@@ -36,42 +36,6 @@ def clique_triangles(edges, number_points, dimension=2):
                 triangles.add(tuple(sorted([i, j, el])))
     return list(triangles)
 
-
-def clique_simplices(edges, number_points, dimension=2):
-    """Return the clique simplices on `edges`
-
-    Parameters
-    ----------
-    edges: list pairs of indices, list of list of int
-        The edges of a graph built on some finite set of points
-    number_points: int
-        The number of vertices of the graph to which the edges belong
-    dimension: int >=2
-        Dimension of clique simplices returned
-
-    Return
-    ------
-    clique_simplices: list of list of int
-        The cliques on the given edges
-
-    """
-    edges = set(edges)
-    all_simplices = list(combinations(range(number_points), dimension+1))
-    edges_in_simplex = list(combinations(range(dimension+1), 2))
-    simplices = []
-    not_found = False
-
-    for sim in all_simplices:
-        not_found = False
-        for e in edges_in_simplex:
-            v1, v2 = sim[e[0]], sim[e[1]]
-            if (v1, v2) not in edges:
-                not_found = True
-                break
-        if not not_found:
-            simplices.append(sim)
-    return simplices
-
 def make_gudhi_simplex_tree(points, edges, max_simplex_dim=2, metric=chebyshev):
     """Returns the `gudhi.SimplexTree()` object containing all simplices up to dimension `max_sim_dim`
 
@@ -81,24 +45,11 @@ def make_gudhi_simplex_tree(points, edges, max_simplex_dim=2, metric=chebyshev):
     """
     sim_tree = SimplexTree()
     vertices = list(range(len(points)))
-
     for v in vertices:
         sim_tree.insert(simplex=[v], filtration=0.0)
     for e in edges:
         p, q = points[e[0]], points[e[1]]
         sim_tree.insert(simplex=e, filtration=metric(p, q))
-
-    for k in range(2, max_simplex_dim+1):
-        if k == 2:
-            simplices = clique_triangles(edges, len(points), k)
-        else:
-            simplices = clique_simplices(edges, len(points), k)
-
-        edges_in_simplices = list(combinations(range(k+1), 2))
-        for sim in simplices:
-            iterable = (metric(points[sim[i]], points[sim[j]])
-                        for i, j in edges_in_simplices)
-            distances = fromiter(iterable, float)
-            sim_tree.insert(simplex=sim, filtration=npmax(distances))
+    sim_tree.expansion(max_simplex_dim)
 
     return sim_tree
